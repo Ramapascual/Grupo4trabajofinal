a12e8a7a1e61a9eb9293d10db0d09bdddf34d8d9
Andrés Ibarra <andresm.ibarrag@unilibrebog.edu.co>
2022-06-15 00:15:32 -0500

    Andrés - Creo estructura de carpetas y retro.md

---
+++ web/src/app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 2022;
+
+app.listen(port, () => console.log('Server listening on port ' + port + '...'));
+
+const publicFolder = path.resolve(__dirname, '../public');
+app.use(express.static(publicFolder));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(publicFolder, 'views/index.html'));
+})
