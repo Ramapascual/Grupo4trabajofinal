d2d6ac9cf4543f208af6d49148a21ce2aed21fa8
Andrés Ibarra <andresm.ibarrag@unilibrebog.edu.co>
2022-08-02 19:52:29 -0500

    Andrés - Reorganizo carpetas para iniciar EJS

--- web/src/app.js
+++ web/src/app.js
@@ -9,29 +9,29 @@ const publicFolder = path.resolve(__dirname, '../public');
 app.use(express.static(publicFolder));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(publicFolder, '../views/index.html'));
+    res.sendFile(path.resolve(publicFolder, './views/index.html'));
 });
 
 app.get('/login.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.get('/register.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 app.post('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 app.post('/login.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.get('/productCart.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/productCart.html'));
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
 
 app.get('/productDetail.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/productDetail.html'));
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
 });
